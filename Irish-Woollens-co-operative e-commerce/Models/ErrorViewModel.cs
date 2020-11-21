a162cd88bd49d6dae99617dbcdf3e0be456c15f9
Kameron Bains <kameronsbains@gmail.com>
2020-11-21 22:40:15 +0000

    Importing project

---
+++ Irish-Woollens-co-operative e-commerce/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Irish_Woollens_co_operative_e_commerce.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
